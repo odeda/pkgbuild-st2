3666fc413a8815c9902501392cc7edf9147e5c85
Oded Adlersberg <adlersberg@gmail.com>
2011-08-31 02:15:32 +0300

    formatting changes

--- PKGBUILD/pkgbuildCommands.py
+++ PKGBUILD/pkgbuildCommands.py
@@ -3,33 +3,33 @@ import sublime
 import sublime_plugin
 
 class MakepkgCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        if self.view.file_name():
-            folder_name, file_name = os.path.split(self.view.file_name())
+  def run(self, edit):
+    if self.view.file_name():
+      folder_name, file_name = os.path.split(self.view.file_name())
 
         print folder_name, file_name
         self.view.window().run_command('exec', {'cmd': ['makepkg', '-c', '-p', file_name], 'working_dir': folder_name, 'quiet': False})
 
 class MakepkgSrcCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        if self.view.file_name():
-            folder_name, file_name = os.path.split(self.view.file_name())
+  def run(self, edit):
+    if self.view.file_name():
+      folder_name, file_name = os.path.split(self.view.file_name())
 
         print folder_name, file_name
         self.view.window().run_command('exec', {'cmd': ['makepkg', '-c', '--source', '-p', file_name], 'working_dir': folder_name, 'quiet': False})
 
 class MakepkgGenCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        if self.view.file_name():
-            folder_name, file_name = os.path.split(self.view.file_name())
+  def run(self, edit):
+    if self.view.file_name():
+      folder_name, file_name = os.path.split(self.view.file_name())
 
         print folder_name, file_name
         self.view.window().run_command('exec', {'cmd': ['makepkg', '-c', '-g', '-p', file_name], 'working_dir': folder_name, 'quiet': False})        
 
 class NamcapCheckCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        if self.view.file_name():
-            folder_name, file_name = os.path.split(self.view.file_name())
+  def run(self, edit):
+    if self.view.file_name():
+      folder_name, file_name = os.path.split(self.view.file_name())
 
         print folder_name, file_name
         self.view.window().run_command('exec', {'cmd': ['namcap', file_name], 'working_dir': folder_name, 'quiet': False})        
